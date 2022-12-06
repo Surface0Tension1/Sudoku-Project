4fee4e142287092598d9621180a8b42e2afd4c7d
MonsieurMantis <116741888+MonsieurMantis@users.noreply.github.com>
2022-12-05 19:53:23 -0500

    Update sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,223 +1,141 @@
-import pygame
-from board import Board
-from board import Cell
-from sudoku_generator import *
-
-pygame.init()
-
-white = (255, 255, 255)
-red = (255, 0, 0)
-grey = (100, 100, 100)
-blue = (0, 0, 128)
-light_blue = (65, 205, 255)
-width = 500
-height = 600
-IncrementW = width/4
-IncrementH = (height-100)/2
-GameButtonHeight = IncrementH * 2.2
-
-screen = pygame.display.set_mode([width, height])
-pygame.display.set_caption('Sudoku')
-font = pygame.font.Font('freesansbold.ttf', 32)
-
-running = True
-main_menu = True
-game_phase = False
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        if event.type == pygame.MOUSEBUTTONDOWN and main_menu:
-            if IncrementW-70 <= mouse[0] <= IncrementW+70 and IncrementH-25 <= mouse[1] <= IncrementH+25:
-                board = Board(9, 9, width, height-100, screen, 30)
-                main_menu = False
-                game_phase = True
-            if IncrementW*2-70 <= mouse[0] <= IncrementW*2+70 and IncrementH-25 <= mouse[1] <= IncrementH+25:
-                board = Board(9, 9, width, height-100, screen, 40)
-                num_array = generate_sudoku(9, 40)
-                for i in range(9):
-                    for j in range(9):
-                        board.cells[i][j].set_cell_value(num_array[i][j])
-                main_menu = False
-                game_phase = True
-            if IncrementW * 3 - 70 <= mouse[0] <= IncrementW*3+70 and IncrementH-25 <= mouse[1] <= IncrementH+25:
-                board = Board(9, 9, width, height-100, screen, 50)
-                num_array = generate_sudoku(9, 50)
-                for i in range(9):
-                    for j in range(9):
-                        board.cells[i][j].set_cell_value(num_array[i][j])
-                main_menu = False
-                game_phase = True
-
-        if event.type == pygame.MOUSEBUTTONDOWN and game_phase:
-            if IncrementW - 70 <= mouse[0] <= IncrementW + 70 and GameButtonHeight - 25 <= mouse[1] <= GameButtonHeight + 25:
-                pass
-                #reset
-            if IncrementW * 2 - 70 <= mouse[0] <= IncrementW * 2 + 70 and GameButtonHeight - 25 <= mouse[1] <= GameButtonHeight + 25:
-                pass
-                game_phase = False
-                main_menu = True
-            if IncrementW * 3 - 70 <= mouse[0] <= IncrementW * 3 + 70 and GameButtonHeight - 25 <= mouse[1] <= GameButtonHeight + 25:
-                pygame.quit()
-
-        mouse = pygame.mouse.get_pos()
-
-        screen.fill(white)
-
-        if width / 4 - 70 <= mouse[0] <= width / 4 + 70 and IncrementH - 25 <= mouse[1] <= IncrementH + 25\
-                and main_menu:
-            # create a text surface object,
-            # on which text is drawn on it.
-            textE = font.render('Easy', True, light_blue, grey)
-
-            # create a rectangular object for the
-            # text surface object
-            textRectE = textE.get_rect()
-            textRectE.center = (width // 4, IncrementH)
-
-            # copying the text surface object
-            # to the display surface object
-            # at the center coordinate.
-            screen.blit(textE, textRectE)
-        elif main_menu:
-            # create a text surface object,
-            # on which text is drawn on it.
-            textE = font.render('Easy', True, blue, grey)
-
-            # create a rectangular object for the
-            # text surface object
-            textRectE = textE.get_rect()
-            textRectE.center = (width // 4, IncrementH)
-
-            # copying the text surface object
-            # to the display surface object
-            # at the center coordinate.
-            screen.blit(textE, textRectE)
-        if width / 2 - 70 <= mouse[0] <= width / 2 + 70 and IncrementH - 25 <= mouse[1] <= IncrementH + 25\
-                and main_menu:
-            # create a text surface object,
-            # on which text is drawn on it.
-            textM = font.render('Medium', True, light_blue, grey)
-
-            # create a rectangular object for the
-            # text surface object
-            textRectM = textM.get_rect()
-            textRectM.center = (width // 2, IncrementH)
-
-            # copying the text surface object
-            # to the display surface object
-            # at the center coordinate.
-            screen.blit(textM, textRectM)
-        elif main_menu:
-            # create a text surface object,
-            # on which text is drawn on it.
-            textM = font.render('Medium', True, blue, grey)
-
-            # create a rectangular object for the
-            # text surface object
-            textRectM = textM.get_rect()
-            textRectM.center = (width // 2, IncrementH)
-
-            # copying the text surface object
-            # to the display surface object
-            # at the center coordinate.
-            screen.blit(textM, textRectM)
-        if width / 4 * 3 - 70 <= mouse[0] <= width / 4*3 + 70 and IncrementH - 25 <= mouse[1] <= IncrementH + 25\
-                and main_menu:
-            # create a text surface object,
-            # on which text is drawn on it.
-            textH = font.render('Hard', True, light_blue, grey)
-
-            # create a rectangular object for the
-            # text surface object
-            textRectH = textH.get_rect()
-            textRectH.center = (width // 4 * 3, IncrementH)
-
-            # copying the text surface object
-            # to the display surface object
-            # at the center coordinate.
-            screen.blit(textH, textRectH)
-        elif main_menu:
-            # create a text surface object,
-            # on which text is drawn on it.
-            textH = font.render('Hard', True, blue, grey)
-
-            # create a rectangular object for the
-            # text surface object
-            textRectH = textH.get_rect()
-            textRectH.center = (width // 4 * 3, IncrementH)
-
-            # copying the text surface object
-            # to the display surface object
-            # at the center coordinate.
-            screen.blit(textH, textRectH)
-        if game_phase:
-            board.draw(screen)
-
-            if width / 4 - 70 <= mouse[0] <= width / 4 + 70 and GameButtonHeight - 25 <= mouse[1] <= GameButtonHeight + 25:
-
-                text_reset = font.render('Reset', True, light_blue, grey)
-
-                textRect_reset = text_reset.get_rect()
-                textRect_reset.center = (width // 4, GameButtonHeight)
-
-                screen.blit(text_reset, textRect_reset)
-
-            elif game_phase:
-                text_reset = font.render('Reset', True, blue, grey)
-
-                textRect_reset = text_reset.get_rect()
-                textRect_reset.center = (width // 4, GameButtonHeight)
-
-                screen.blit(text_reset, textRect_reset)
-
-            if width / 2 - 70 <= mouse[0] <= width / 2 + 70 and GameButtonHeight - 25 <= mouse[1] <= GameButtonHeight + 25 \
-                    and game_phase:
-
-                text_restart = font.render('Restart', True, light_blue, grey)
-
-                textRect_restart = text_restart.get_rect()
-                textRect_restart.center = (width // 2, GameButtonHeight)
-
-                screen.blit(text_restart, textRect_restart)
-
-            elif game_phase:
-                text_restart = font.render('Restart', True, blue, grey)
-
-                textRect_restart = text_restart.get_rect()
-                textRect_restart.center = (width // 2, GameButtonHeight)
-
-                screen.blit(text_restart, textRect_restart)
-
-            if width / 4 * 3 - 70 <= mouse[0] <= width / 4 * 3 + 70 and GameButtonHeight - 25 <= mouse[1] <= GameButtonHeight + 25 \
-                    and game_phase:
-
-                text_quit = font.render('Quit', True, light_blue, grey)
-
-                textRect_quit = text_quit.get_rect()
-                textRect_quit.center = (width // 4 * 3, GameButtonHeight)
-
-                screen.blit(text_quit, textRect_quit)
-
-            elif game_phase:
-                text_quit = font.render('Quit', True, blue, grey)
-
-                textRect_quit = text_quit.get_rect()
-                textRect_quit.center = (width // 4 * 3, GameButtonHeight)
-
-                screen.blit(text_quit, textRect_quit)
+import math, random
 
 
+class SudokuGenerator:
+    '''
+	create a sudoku board - initialize class variables and set up the 2D board
+	This should initialize:
+	self.row_length		- the length of each row
+	self.removed_cells	- the total number of cells to be removed
+	self.board			- a 2D list of ints to represent the board
+	self.box_length		- the square root of row_length
+
+	Parameters:
+    row_length is the number of rows/columns of the board (always 9 for this project)
+    removed_cells is an integer value - the number of cells to be removed
+
+	Return:
+	None
+    '''
+
+    def __init__(self, row_length, removed_cells):
+        self.row_length = row_length
+        self.removed = removed_cells
+        self.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        self.box_length = 3
+
+    def get_board(self):
+        return self.board
+
+    def print_board(self):
+        for row in self.board:
+            for col in row:
+                print(col, end=' ')
+            print()
+
+    def valid_in_row(self, row, num):
+        if num in self.get_board()[row]:
+            return False
+        else:
+            return True
+
+    def valid_in_col(self, col, num):
+        for x in range(9):
+            if num == self.get_board()[x][col]:
+                return False
+        return True
+
+    def valid_in_box(self, row_start, col_start, num):
+        for r in range(row_start, row_start + 3):
+            for c in range(col_start, col_start + 3):
+                if self.get_board()[r][c] == num:
+                    return False
+        return True
+
+    def is_valid(self, row, col, num):
+        if self.valid_in_row(row, num):
+            if self.valid_in_col(col, num):
+                if self.valid_in_box(row//3*3, col//3*3, num):
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
+    def fill_box(self, row_start, col_start):
+        for row in range(row_start, row_start + 3):
+            for col in range(col_start, col_start + 3):
+                num = random.randint(1, 9)
+                while not self.valid_in_box(row_start, col_start, num):
+                    num = random.randint(1, 9)
+                self.board[row][col] = num
+
+    def fill_diagonal(self):
+        self.fill_box(0, 0)
+        self.fill_box(3, 3)
+        self.fill_box(6, 6)
+
+    def fill_remaining(self, row, col):
+        if col >= self.row_length and row < self.row_length - 1:
+            row += 1
+            col = 0
+        if row >= self.row_length and col >= self.row_length:
+            return True
+        if row < self.box_length:
+            if col < self.box_length:
+                col = self.box_length
+        elif row < self.row_length - self.box_length:
+            if col == int(row // self.box_length * self.box_length):
+                col += self.box_length
+        else:
+            if col == self.row_length - self.box_length:
+                row += 1
+                col = 0
+                if row >= self.row_length:
+                    return True
+
+        for num in range(1, self.row_length + 1):
+            if self.is_valid(row, col, num):
+                self.board[row][col] = num
+                if self.fill_remaining(row, col + 1):
+                    return True
+                self.board[row][col] = 0
+        return False
+
+    def fill_values(self):
+        self.fill_diagonal()
+        self.fill_remaining(0, self.box_length)
+
+    def remove_cells(self):
+        all_coords = []
+        for i in range(self.removed):
+            x = random.randint(0, 8)
+            y = random.randint(0, 8)
+            coords = (x, y)
+            while coords in all_coords:
+                x = random.randint(0, 8)
+                y = random.randint(0, 8)
+                coords = (x, y)
+            all_coords.append(coords)
+            self.board[y][x] = 0
 
 
-
-        if event.type == pygame.MOUSEBUTTONDOWN and not main_menu:
-            x, y = board.click(mouse[0], mouse[1])
-            if mouse[1] <= 500:
-                board.select(x, y)
-        pygame.display.flip()
-
-pygame.quit()
+def generate_sudoku(size, removed):
+    sudoku = SudokuGenerator(size, removed)
+    sudoku.fill_values()
+    sudoku.remove_cells()
+    board = sudoku.get_board()
+    return board
 
